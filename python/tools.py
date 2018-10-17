21ea25e8534d2479b8f9aca279a7483c56234801
Daniel Marley <daniel.edison.marley@cern.ch>
2018-10-17 13:29:43 -0500

    Further refinements to re-binning with numpy histograms

--- python/tools.py
+++ python/tools.py
@@ -1,19 +1,19 @@
 """
 Created:         1 September 2016
-Last Updated:   12 September 2018
+Last Updated:   17 October   2018
 
 Dan Marley
 daniel.edison.marley@cernSPAMNOT.ch
 Texas A&M University
 -----
 
-Simple functions to help with plotting
+Simple functions to help with plotting & accessing data
 """
 import numpy as np
 from array import array
 
 
-m_scipy_available  = False  # for 2D rebinning
+m_scipy_available  = False  # for re-binning 2D histograms
 m_ROOT_available   = False
 m_uproot_available = False
 
@@ -22,23 +22,26 @@ try:
     m_scipy_available = True
 except ImportError:
     m_scipy_available = False
+    print ' WARNING : Scipy not available, may cause issues with re-binning 2D histograms.'
 
 try:
     import ROOT
     m_ROOT_available = True
 except ImportError:
     m_ROOT_available = False
-    print " 'CERN ROOT' not available, trying 'uproot'"
     try:
         import uproot as up
         m_uproot_available = True
     except ImportError:
         m_uproot_available = False
-        print " 'uproot' not available.  Will not be able to load ROOT histograms"
+        print " WARNING : Neither 'uproot' nor 'CERN ROOT' available."
+        print "         : Will not be able to load ROOT histograms."
 
 if m_ROOT_available and m_uproot_available:
-    print " Both 'CERN ROOT' and 'uproot' available; defaulting to 'uproot'"
+    print " INFO : Both 'CERN ROOT' and 'uproot' available. Defaulting to 'uproot'."
     m_ROOT_available = False
+
+
 
 
 
@@ -147,7 +150,10 @@ def data2list2D(data,weights=None,normed=False,binning=1):
 
 
 def hist2list(histo,name='',normed=False,reBin=-1):
-    """Convert ROOT histogram to (dictionary of) lists"""
+    """
+    Convert ROOT histogram to (dictionary of) lists.
+    For re-binning, try to mirror CERN ROOT functionality with numpy histograms.
+    """
     bin_centers  = []
     bin_contents = []
     bin_errors   = []
@@ -163,14 +169,26 @@ def hist2list(histo,name='',normed=False,reBin=-1):
         bin_centers = midpoints(bin_edges)
 
         if reBin is not None:
+            if isinstance(reBin, (int,long)):
+                # merge bins together by some integer factor 'reBin'
+                if reBin<=0 or bin_contents.size%reBin:
+                    print " WARNING : Cannot re-bin with {0}".format(reBin)
+                    print "         : Not re-binning the histogram"
+                    reBin = bin_edges.copy()
+                else:
+                    reBin = np.array( [bin_edges[i] for i in range(0,bin_edges.size,reBin)] )
+            elif not all((i in bin_edges) for i in reBin):
+                print " WARNING : Cannot re-bin with {0}".format(reBin)
+                print "         : Not re-binning the histogram"
+                reBin = bin_edges.copy()
+
             # re-bin the histogram (by making a new histogram)
-            # - let numpy handle the type of 'reBin'
+            # with variable binning -- edges must still line up!
             bin_contents,bin_edges = np.histogram(bin_centers,bins=reBin,weights=bin_contents,normed=normed)
-            bin_centers = midpoints(bin_edges)
-            nbin_edges  = len(bin_edges)
+            nbin_edges = len(bin_edges)
             if not histo.variances:
                 # re-bin bin errors
-                bin_index   = np.digitize(bin_centers, bin_edges) # values start at 1, not 0
+                bin_index   = np.digitize(bin_centers, bin_edges)  # find where original values migrated in new binning
                 bin_weights = np.asarray([bin_errors[np.where(bin_index==idx)[0]] for idx in range(1,nbin_edges)])
                 bin_errors  = np.sqrt( np.sum(np.square(binned_weights)) )
             else:
@@ -201,7 +219,7 @@ def hist2list(histo,name='',normed=False,reBin=-1):
 			bin_centers.append(histo.GetBinCenter(i))
 			bin_contents.append(histo.GetBinContent(i))
 			bin_errors.append(histo.GetBinError(i))
-			bin_widths.append(histo.GetXaxis().GetBinWidth(i)/2.)
+			bin_widths.append(histo.GetXaxis().GetBinWidth(i)*0.5)
 
     results = Data()
     results.content = np.array(bin_contents)
@@ -215,7 +233,10 @@ def hist2list(histo,name='',normed=False,reBin=-1):
 
 
 def hist2list2D(histo,name='',reBin=None,normed=False):
-    """Convert ROOT histogram to list for 2D plots."""
+    """
+    Convert ROOT histogram to list for 2D plots.
+    For re-binning, try to mirror CERN ROOT functionality with numpy histograms.
+    """
     bin_centers  = {'x':[],'y':[]}
     bin_contents = []
     bin_errors   = []
@@ -229,41 +250,66 @@ def hist2list2D(histo,name='',reBin=None,normed=False):
         bin_contents,(xbin_edges,ybin_edges) = histo.numpy()
         bin_errors = histo.variances if histo.variances else bin_contents
 
-        # create dummy binning for pseudo-data
-        mbins_x = midpoints(xbin_edges)  # get midpoints of bins given the bin edges
-        mbins_y = midpoints(ybin_edges)
-
         if reBin is not None:
             # re-bin the histogram (by making a new histogram)
-            # - let numpy handle the type of 'reBin'
+            xrebin = xbin_edges.copy() # default to original binning
+            yrebin = ybin_edges.copy() # default to original binning
+            if isinstance(reBin, (int,long)):
+                # new bins should be merged bin edges
+                if reBin<=0 or bin_contents[0].size%reBin or bin_contents[1].size%reBin:
+                    print " WARNING : Cannot re-bin with {0}".format(reBin)
+                    print "         : Not re-binning the histogram"
+                else:
+                    xrebin = np.array([xbin_edges[i] for i in range(0,xbin_edges.size,reBin)] )
+                    yrebin = np.array([ybin_edges[i] for i in range(0,ybin_edges.size,reBin)] )
+            else:
+                # new bins should be new array bin edges
+                # - edges must still match previous binning, e.g.,
+                #   can't re-bin [0,1,4,6] into [0,3,6]
+                try: 
+                    xrebin = reBin['x']
+                    yrebin = reBin['y']
+                except TypeError:
+                    xrebin = reBin[0]
+                    yrebin = reBin[1]
+                if (not all((i in xbin_edges) for i in xrebin)) or (not all((i in ybin_edges) for i in yrebin)):
+                    print " WARNING : Cannot re-bin 2D histogram using {0}".format(reBin)
+                    print "         : Not re-binning histogram"
+
             # - generate dummy values of the bin centers and pass data as weights
-            xbins = mbins_x.repeat(len(mbins_y))
-            ybins = np.tile(mbins_y, (1,len(mbins_x)))[0]
-            rb_hist2d = np.histogram2d(xbins,ybins,bins=reBin,normed=normed,\
+            ymbins = midpoints(xbin_edges)
+            xmbins = midpoints(ybin_edges)
+            xbins  = xmbins.repeat(len(ymbins))
+            ybins  = np.tile(ymbins, (1,len(xmbins)))[0]
+
+            rb_hist2d = np.histogram2d(xbins,ybins,bins=[xrebin,yrebin],normed=normed,\
                                        weights=bin_contents.flatten())
             bin_contents,xbin_edges,ybin_edges = rb_hist2d
-            mbins_x = midpoints(xbin_edges)  # get midpoints of bins given the bin edges
-            mbins_y = midpoints(ybin_edges)
-            xbins_unravel = len(xbin_edges)+1
-            ybins_unravel = len(ybin_edges)+1
-            nbin_edges    = xbins_unravel*ybins_unravel
+
+            nxbins = len(xbin_edges)-1
+            nybins = len(ybin_edges)-1
+            xbins_unravel = nxbins+2
+            ybins_unravel = nybins+2
             if not histo.variances and m_scipy_available:
-                # re-bin bin errors
-                s,bx,by,bin_index = scipy.stats.binned_statistic_2d(x,y,x,bins=10)
+                # rebin the histogram to see where the old bins get merged into new bins
+                s,bx,by,bin_index = scipy.stats.binned_statistic_2d(xbins,ybins,xbins,bins=[xrebin,yrebin])
                 # convert scipy bin_index to bin_index of given array (scipy includes over/underflow)
-                bin_weights = []
-                bin_index = np.unravel_index(bin_index,(xbins_unravel,ybins_unravel))
-                for ix in range(xbins_unravel):
-                    for iy in range(ybins_unravel):
-                        bin_weights.append(bin_errors[np.where(bin_index==idx)[0]])
-                bin_errors  = np.sqrt( np.sum(np.square(binned_weights)) )
+                unrv_bc = np.unravel_index(bin_index,[xbins_unravel,ybins_unravel])
+                bin_idx = np.array(unrv_bc).T-1      # convert unrv_bc from tuple to 2D array
+                # combine weights in quadrature into array
+                bin_errors = np.zeros(nxbins*nybins).reshape(nxbins,nybins) # same shape as hist
+                for ix in range(nxbins):
+                    for iy in range(nybins):
+                        matches = [ib==[ix,iy] for ib in bin_idx.tolist()]
+                        bin_errors[ix][iy] = np.sqrt( np.sum(np.square(e[matches])) )
             else:
-                bin_errors  = bin_contents
+                print " WARNING :: Tools : Either no scipy or variances, just use re-binned content"
+                bin_errors = bin_contents
 
         bin_contents = bin_contents.flatten()
-        bin_edges    = {'x':xbin_edges,'y':ybin_edges}
         bin_errors   = bin_errors.flatten()
-        bin_centers  = {'x':mbins_x,'y':mbins_y}
+        bin_edges    = {'x':xbin_edges,'y':ybin_edges}
+        bin_centers  = {'x':midpoints(xbin_edges),'y':midpoints(ybin_edges)}
         bin_widths   = {'x':xbin_edges[1:]-xbin_edges[:-1],'y':ybin_edges[1:]-ybin_edges[:-1]}
     elif m_ROOT_available:
         if not histo.GetSumw2N():
@@ -280,9 +326,15 @@ def hist2list2D(histo,name='',reBin=None,normed=False):
             except TypeError:
                 newname   = histo.GetName()+"_"+name
                 old_histo = histo.Clone()             # special rebinning, redefine histo
-                new_x     = reBin['x']
-                new_y     = reBin['y']
-                histo     = ROOT.TH2F(old_histo.GetName()+newname,old_histo.GetTitle()+newname,len(new_x)-1,new_x,len(new_y)-1,new_y)
+                try:
+                    new_x = reBin['x']
+                    new_y = reBin['y']
+                except KeyError:
+                    print " WARNING : Need to pass new bins for re-binning as a dictionary"
+                    print "         : Not applying re-binning, please check your setup."
+                    new_x = old_histo.GetXaxis().GetArray() # not sure if this works
+                    new_y = old_histo.GetYaxis().GetArray()
+                histo = ROOT.TH2F(old_histo.GetName()+newname,old_histo.GetTitle()+newname,len(new_x)-1,new_x,len(new_y)-1,new_y)
                 xaxis = old_histo.GetXaxis()
                 yaxis = old_histo.GetYaxis()
                 for i in xrange(1,xaxis.GetNbins()):
@@ -300,8 +352,8 @@ def hist2list2D(histo,name='',reBin=None,normed=False):
                 bin_errors.append(histo.GetBinError(i,j))
                 bin_centers['x'].append(histo.GetXaxis().GetBinCenter(i))
                 bin_centers['y'].append(histo.GetYaxis().GetBinCenter(j))
-                bin_widths['x'].append(histo.GetXaxis().GetBinWidth(i)/2.)
-                bin_widths['y'].append(histo.GetYaxis().GetBinWidth(i)/2.)
+                bin_widths['x'].append(histo.GetXaxis().GetBinWidth(i)*0.5)
+                bin_widths['y'].append(histo.GetYaxis().GetBinWidth(i)*0.5)
 
     results = Data()
     results.content = np.array(bin_contents)
